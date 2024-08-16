dc83235463d073e8d76d78fb9a7eb64a8306bacb
Joël Gregoire <joel.gregoire@ubisoft.com>
2024-08-16 15:55:36 +0200

    remove code logic from the foward pass

--- src/unet.py
+++ src/unet.py
@@ -29,6 +29,13 @@ class ResidualConvBlock(nn.Module):
             nn.GELU(),   # GELU activation function
         )
 
+        # If input and output channels are the same, add residual connection directly
+        if self.is_res and not self.same_channels:
+            self.shortcut = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0)
+        else:
+            # If not, apply a 1x1 convolutional layer to match dimensions before adding residual connection
+            self.shortcut = nn.Identity()
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
 
         # If using residual connection
@@ -39,13 +46,8 @@ class ResidualConvBlock(nn.Module):
             # Apply second convolutional layer
             x2 = self.conv2(x1)
 
-            # If input and output channels are the same, add residual connection directly
-            if self.same_channels:
-                out = x + x2
-            else:
-                # If not, apply a 1x1 convolutional layer to match dimensions before adding residual connection
-                shortcut = nn.Conv2d(x.shape[1], x2.shape[1], kernel_size=1, stride=1, padding=0).to(x.device)
-                out = shortcut(x) + x2
+            out = self.shortcut(x) + x2
+
             log.debug(f"resconv forward: x {x.shape}, x1 {x1.shape}, x2 {x2.shape}, out {out.shape}")
 
             # Normalize output tensor
